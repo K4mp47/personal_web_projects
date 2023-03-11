d102a2e38a49afc4a2dbc67913fb6d85e85a2d41
Hack-one-creator <67239535+Hack-one-creator@users.noreply.github.com>
2023-03-11 13:47:35 +0100

    adding animation
    added scroll animation for all the page

---
+++ portfolio/first_try/app.js
@@ -0,0 +1,17 @@
+/**
+ * a beautiful scroll animation
+ * thanks to fireship for the native code!
+ */
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
